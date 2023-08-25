5b862fea627e4344c9c80eba47047f9da299162f
Parviz Shirinov <parveeezy@gmail.com>
2023-08-25 16:42:13 +0500

    Initial commit
    (secret op ?)

---
+++ components/game/game-symbol.jsx
@@ -0,0 +1,14 @@
+import {SYMBOL_X, SYMBOL_O} from './constants.js'
+import styles from './game.module.css'
+
+export function GameSymbol({ symbol }) {
+    const getSymbolClassName = (symbol) => {
+      if (symbol === SYMBOL_O) return styles["symbol--o"];
+      if (symbol === SYMBOL_X) return styles["symbol--x"];
+      return "";
+    };
+    return (
+      <span className={`${styles['symbol']} ${getSymbolClassName(symbol)}`}>{symbol}</span>
+    );
+  }
+  
